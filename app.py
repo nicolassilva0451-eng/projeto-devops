f67da9241a338d952662b16ac77f85b5d6f47a27
nicolassilva0451-eng <nicolassilva0451@gmail.com>
2026-09-19 12:59:28 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,19 @@
 def soma(a, b):
     return a + b
 
+def subtracao(a, b):
+    return a - b
+
+def multiplicacao(a, b):
+    return a * b
+
+def divisao(a, b):
+    if b == 0:
+        raise ValueError("Divisão por zero não permitida")
+    return a / b
+
+def eh_par(n):
+    return n % 2 == 0
+
 if __name__ == "__main__":
     print("Projeto DevOps funcionando!")
